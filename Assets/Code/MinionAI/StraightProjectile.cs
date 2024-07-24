43c70d35818f304b4c8e3ac974905aed0616529d
Gacsam <gacsam@ymail.com>
2024-07-24 18:18:06 +0100

    Update StraightProjectile.cs
    Stopped damaging allied units

--- Assets/Code/MinionAI/StraightProjectile.cs
+++ Assets/Code/MinionAI/StraightProjectile.cs
@@ -16,17 +16,20 @@ namespace Assets.Code.MinionAI
             if (objectThatWasHit.GetComponent<BaseObject>() != null)
             {
                 var theUnitHit = objectThatWasHit.GetComponent<BaseObject>();
-                // - cause we want to modify health by a negative value
-                theUnitHit.ModifyHealth(-damage);
-                // If not piercing destroy self
-                if (!isPiercing)
+                if (theUnitHit.thisUnitSide != allySide)
                 {
-                    Destroy(this.gameObject);
-                }
-                // If is piercing continue until tower? Probably OP should add a timer
-                else if (theUnitHit.thisUnitType == UnitType.Tower)
-                {
-                    Destroy(this.gameObject);
+                    // - cause we want to modify health by a negative value
+                    theUnitHit.ModifyHealth(-damage);
+                    // If not piercing destroy self
+                    if (!isPiercing)
+                    {
+                        Destroy(this.gameObject);
+                    }
+                    // If is piercing continue until tower? Probably OP should add a timer
+                    else if (theUnitHit.thisUnitType == UnitType.Tower)
+                    {
+                        Destroy(this.gameObject);
+                    }
                 }
             }
         }
